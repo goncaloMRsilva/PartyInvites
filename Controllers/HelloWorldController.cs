87d88ca8e4828466a5701a5fb1bf5fbb1e8e4c4a
1703826 <gmrs99.silva@gmail.com>
2021-10-13 09:41:24 +0100

    Programa Hello World

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/Index
+        public IActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+
+            if (hour >= 7 && hour < 12)
+            {
+                ViewBag.Mesasge = "Goood Morning";
+            }
+            else if (hour >= 12 && hour < 20)
+            {
+                ViewBag.Mesasge = "Good Afternoon";
+            }
+            else
+            {
+                ViewBag.Mesasge = "Good Night";
+            }
+            
+
+            ViewBag.Mesasge = "Olá";
+            return View();
+        }
+
+        //
+        // GET: /HelloWorld/Welcome
+        public string Welcome()
+        {
+            return "This is my Welcome action method...";
+        }
+    }
+}
